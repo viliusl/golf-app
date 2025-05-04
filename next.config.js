a78d5d2bafcda9d23ee9309a2c1f193fadabee15
Vilius Lukosius <vilius@wix.com>
2025-05-04 16:56:03 +0300

    Update Next.js config with basic auth environment variables

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    BASIC_AUTH_USERNAME: 'modestas',
+    BASIC_AUTH_PASSWORD: 'TMog7pdkfpQM',
+  },
 };
 
 module.exports = nextConfig; 
