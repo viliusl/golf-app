abca8a03f0da7f69ea960ba7a34a275449bae5dd
Vilius Lukosius <vilius@wix.com>
2025-05-19 09:03:42 +0300

    hole 12: par5 -> par4

--- src/app/(app)/events/[id]/matches/add/page.tsx
+++ src/app/(app)/events/[id]/matches/add/page.tsx
@@ -92,7 +92,7 @@ export default function AddMatch({ params }: { params: { id: string } }) {
     { hole: 9, handicap: 3, par: 3, pace: 12, player1Score: 0, player2Score: 0, player1Putt: false, player2Putt: false, winner: 'tie' },
     { hole: 10, handicap: 12, par: 5, pace: 17, player1Score: 0, player2Score: 0, player1Putt: false, player2Putt: false, winner: 'tie' },
     { hole: 11, handicap: 2, par: 4, pace: 15, player1Score: 0, player2Score: 0, player1Putt: false, player2Putt: false, winner: 'tie' },
-    { hole: 12, handicap: 14, par: 5, pace: 17, player1Score: 0, player2Score: 0, player1Putt: false, player2Putt: false, winner: 'tie' },
+    { hole: 12, handicap: 14, par: 4, pace: 17, player1Score: 0, player2Score: 0, player1Putt: false, player2Putt: false, winner: 'tie' },
     { hole: 13, handicap: 18, par: 3, pace: 12, player1Score: 0, player2Score: 0, player1Putt: false, player2Putt: false, winner: 'tie' },
     { hole: 14, handicap: 4, par: 5, pace: 17, player1Score: 0, player2Score: 0, player1Putt: false, player2Putt: false, winner: 'tie' },
     { hole: 15, handicap: 8, par: 4, pace: 15, player1Score: 0, player2Score: 0, player1Putt: false, player2Putt: false, winner: 'tie' },
